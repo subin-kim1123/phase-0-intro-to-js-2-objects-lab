ef3e80e6b9d85772759bd3de1207d6e6efb6a14f
Subin Kim <https://github.com/subin-kim1123>
2021-04-19 05:08:26 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Write your solution in this file!
+const employee = {
+    name: 'Sue',
+    streetAddress: '1172 Broadway'
+}
+
+function updateEmployeeWithKeyAndValue(employee,{name, streetAddress}) {
+    const employees = {
+        name : 'Sam',
+        streetAddress: '11 Broadway'
+    }
+    return employees;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, {name= 'Sam'}){
+    const employees = {
+        name : 'Sam',
+        streetAddress: '12 Broadway'
+    }
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee,{name, streetAddress}) {
+    const employees = {
+        name : 'Sam',
+        streetAddress: '11 Broadway'
+    }
+    delete employees.name;
+    return employees;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee,{name, streetAddress}) {
+    const employees = {
+        name : 'Sam',
+        streetAddress: '11 Broadway'
+    }
+    return employee;
+}
+
